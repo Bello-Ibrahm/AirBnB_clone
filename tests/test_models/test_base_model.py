290f3c3051d29f7673f49ed2fe36488427861dbe
an-nur-info-tech <wasiubello60@gmail.com>
2023-11-08 15:28:01 -0500

    Update Base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,18 +2,30 @@
 """Define unittests for the models/base_model.py"""
 import os
 import unittest
+import models
+import json
+from uuid import UUID
 from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
     """Unittests for testing ation of the BaseModel"""
-    def __init__(self):
-        """Init the test class of BaseModel"""
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
+
+    def test_setUp(self):
+        """The setup method of the test class"""
+        pass
+
+    def test_tearDown(self):
+        """teardown method of the class test"""
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
 
     def test_no_args(self):
         """Unittest for testing instantiation with no args"""
         self.assertEqual(BaseModel, type(BaseModel()))
 
-
+    def test_pub_id_str(self):
+        """Unittest if public id is a string"""
+        self.assertEqual(str, type(BaseModel().id))
