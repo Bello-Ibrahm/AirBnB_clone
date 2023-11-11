468487b620dcc770fd6e5f998e5d797e9fd8d9b2
an-nur-info-tech <wasiubello60@gmail.com>
2023-11-11 08:18:24 -0500

    Add console file

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Console module"""
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class for HBNB"""
+
+    # Check for interactive/non-interactive mode
+    prompt = "(hbnb) " if sys.__stdin__.isatty() ""
+
+    def do_EOF(self, arg):
+        """Handle EOF to exit program"""
+        return True
+
+    def emptyline(self):
+        """Override the cmd with empty line"""
+        pass
+
+    def do_quit(self, arg):
+        """Exit the command"""
+        return True
+
+    def help_quit(self):
+        """Prints out the help doc for quit"""
+        print("Quit command to exit program\n")
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
